a5afa5e6dd2c2b28c4255adf3107d46e76f597e1
osamikoyo <v.pylayev@mail.ru>
2025-11-20 07:09:51 +0500

    update release

--- services/music/entity/release.go
+++ services/music/entity/release.go
@@ -1,18 +1,36 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"github.com/osamikoyo/music-and-marks/services/music/api/proto/gen/pb"
+)
 
 type Release struct {
-    ID          string    `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
-    MBID        string    `gorm:"uniqueIndex;size:36;not null" json:"mbid"`
-    Title       string    `gorm:"type:text;not null" json:"title"`
-    ReleaseGroupID string `gorm:"type:uuid;index" json:"release_group_id"`
-    ReleaseGroup   ReleaseGroup `gorm:"foreignKey:ReleaseGroupID;references:ID" json:"release_group,omitempty"`
-    Status      string    `gorm:"type:text" json:"status,omitempty"`
-    Country     string    `gorm:"size:2" json:"country,omitempty"`
-    Date        *string   `gorm:"type:date" json:"date,omitempty"`
-    Format      string    `gorm:"type:text" json:"format,omitempty"` 
-    TrackCount  int       `gorm:"default:0" json:"track_count,omitempty"`
-    CreatedAt   time.Time `gorm:"autoCreateTime" json:"-"`
-    UpdatedAt   time.Time `gorm:"autoUpdateTime" json:"-"`
+	ID             string       `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	MBID           string       `gorm:"uniqueIndex;size:36;not null" json:"mbid"`
+	Title          string       `gorm:"type:text;not null" json:"title"`
+	ReleaseGroupID string       `gorm:"type:uuid;index" json:"release_group_id"`
+	ReleaseGroup   ReleaseGroup `gorm:"foreignKey:ReleaseGroupID;references:ID" json:"release_group"`
+	Status         string       `gorm:"type:text" json:"status,omitempty"`
+	Country        string       `gorm:"size:2" json:"country,omitempty"`
+	Date           *string      `gorm:"type:date" json:"date,omitempty"`
+	Format         string       `gorm:"type:text" json:"format,omitempty"`
+	TrackCount     int          `gorm:"default:0" json:"track_count,omitempty"`
+	CreatedAt      time.Time    `gorm:"autoCreateTime" json:"-"`
+	UpdatedAt      time.Time    `gorm:"autoUpdateTime" json:"-"`
+}
+
+func (r *Release) ToPB() *pb.Release {
+	return &pb.Release{
+		Id:             r.ID,
+		Mbid:           r.MBID,
+		Title:          r.Title,
+		ReleaseGroupId: r.ReleaseGroupID,
+		Status:         &r.Status,
+		Country:        &r.Country,
+		Date:           r.Date,
+		Format:         &r.Format,
+		TrackCount:     int32(r.TrackCount),
+	}
 }
