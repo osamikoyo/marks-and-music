ecb28d029f31d36e6ba0826222f3a35ab56f522a
osamikoyo <v.pylayev@mail.ru>
2025-11-20 07:09:41 +0500

    update music core

--- services/music/core/core.go
+++ services/music/core/core.go
@@ -14,13 +14,15 @@ const DefaultPageSize = 10
 var (
 	ErrEmptyField = errors.New("empty field")
 	ErrUIDFailed  = errors.New("failed parse uid")
+	ErrPageIndex  = errors.New("page index less than zero")
 )
 
 type Repository interface {
 	GetArtistByID(ctx context.Context, id uuid.UUID) (*entity.Artist, error)
 	GetReleaseByID(ctx context.Context, id uuid.UUID) (*entity.Release, error)
-	Search(ctx context.Context, query string, page_size, page_index int) ([]entity.AlbumSearchResult, error)
+	Search(ctx context.Context, query string, page_size, page_index int) ([]entity.SearchResult, error)
 	ReadArtists(ctx context.Context, page_size, page_index int) ([]entity.Artist, error)
+	ReadReleases(ctx context.Context, page_size, page_index int) ([]entity.Release, error)
 }
 
 type Fetcher interface {
@@ -83,4 +85,44 @@ func (mc *MusicCore) Search(query string, page_index int) ([]entity.AlbumSearchR
 	mc.fetcher.AsyncFetch(query)
 
 	return nil, errors.New("not found results in db, fetching")
+}
+
+func (mc *MusicCore) ReadArtists(page_size, page_index int) ([]entity.Artist, error) {
+	if page_size == 0 {
+		return nil, nil
+	}
+
+	if page_index < 0 {
+		return nil, ErrPageIndex
+	}
+
+	ctx, cancel := mc.context()
+	defer cancel()
+
+	artists, err := mc.repo.ReadArtists(ctx, page_size, page_index)
+	if err != nil {
+		return nil, err
+	}
+
+	return artists, nil
+}
+
+func (mc *MusicCore) ReadReleases(page_size, page_index int) ([]entity.Release, error) {
+	if page_size == 0 {
+		return nil, nil
+	}
+
+	if page_index < 0 {
+		return nil, ErrPageIndex
+	}
+
+	ctx, cancel := mc.context()
+	defer cancel()
+
+	albums, err := mc.repo.ReadReleases(ctx, page_size, page_index)
+	if err != nil {
+		return nil, err
+	}
+
+	return albums, nil
 }
