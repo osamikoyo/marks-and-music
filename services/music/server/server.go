9c74fda8591cc0009d1b3d7a2c60051a69d5dbfe
osamikoyo <v.pylayev@mail.ru>
2025-11-23 07:04:14 +0500

    update server

--- services/music/server/server.go
+++ services/music/server/server.go
@@ -7,11 +7,10 @@ import (
 	"github.com/osamikoyo/music-and-marks/logger"
 	"github.com/osamikoyo/music-and-marks/services/music/api/proto/gen/pb"
 	"github.com/osamikoyo/music-and-marks/services/music/core"
+	"go.uber.org/zap"
 )
 
-var (
-	ErrEmptyRequest = errors.New("request is empty")
-)
+var ErrEmptyRequest = errors.New("request is empty")
 
 type Server struct {
 	pb.UnimplementedMusicServiceServer
@@ -42,12 +41,12 @@ func (s *Server) GetArtist(ctx context.Context, req *pb.GetArtistRequest) (*pb.G
 }
 
 func (s *Server) GetRelease(ctx context.Context, req *pb.GetReleaseRequest) (*pb.GetReleaseResponse, error) {
-	if req == nil{
+	if req == nil {
 		return nil, ErrEmptyRequest
 	}
 
 	release, err := s.core.GetRelease(req.Id)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
@@ -57,13 +56,75 @@ func (s *Server) GetRelease(ctx context.Context, req *pb.GetReleaseRequest) (*pb
 }
 
 func (s *Server) Search(ctx context.Context, req *pb.SearchRequest) (*pb.SearchResponse, error) {
+	if req == nil {
+		return nil, ErrEmptyRequest
+	}
 
+	results, err := s.core.Search(req.Query, int(req.PageIndex), int(req.PageSize))
+	if err != nil {
+		s.logger.Error("failed search",
+			zap.String("query", req.Query),
+			zap.Error(err))
+
+		return nil, err
+	}
+
+	pbresults := make([]*pb.SearchResult, len(results))
+	for i, result := range results {
+		pbresults[i] = result.ToPB()
+	}
+
+	return &pb.SearchResponse{
+		Results: pbresults,
+	}, nil
 }
 
 func (s *Server) ReadArtists(ctx context.Context, req *pb.ReadArtistsRequest) (*pb.ReadArtistsResponse, error) {
+	if req == nil {
+		return nil, ErrEmptyRequest
+	}
 
+	artists, err := s.core.ReadArtists(int(req.PageSize), int(req.PageIndex))
+	if err != nil {
+		s.logger.Error("failed read artists",
+			zap.Int("page_size", int(req.PageSize)),
+			zap.Int("page_index", int(req.PageIndex)),
+			zap.Error(err))
+
+		return nil, err
+	}
+
+	pbartists := make([]*pb.Artist, len(artists))
+	for i, artist := range artists {
+		pbartists[i] = artist.ToPB()
+	}
+
+	return &pb.ReadArtistsResponse{
+		Artists: pbartists,
+	}, nil
 }
 
 func (s *Server) ReadReleases(ctx context.Context, req *pb.ReadReleasesRequest) (*pb.ReadReleasesResponse, error) {
+	if req == nil {
+		return nil, ErrEmptyRequest
+	}
 
+	releases, err := s.core.ReadReleases(int(req.PageSize), int(req.PageIndex))
+	if err != nil {
+		s.logger.Error("failed read releases",
+			zap.Int("page_size", int(req.PageSize)),
+			zap.Int("page_index", int(req.PageIndex)),
+			zap.Error(err))
+
+		return nil, err
+	}
+
+	pbreleases := make([]*pb.Release, len(releases))
+	for i, release := range releases {
+		pbreleases[i] = release.ToPB()
+	}
+
+	return &pb.ReadReleasesResponse{
+		Releases: pbreleases,
+	}, nil
 }
