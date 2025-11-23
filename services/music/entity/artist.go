408cf528eab76eae2296c408137622355c82864a
osamikoyo <v.pylayev@mail.ru>
2025-11-23 07:05:22 +0500

    update artist entity

--- services/music/entity/artist.go
+++ services/music/entity/artist.go
@@ -2,14 +2,26 @@ package entity
 
 import (
 	"time"
+
+	"github.com/osamikoyo/music-and-marks/services/music/api/proto/gen/pb"
 )
 
 type Artist struct {
-    ID             string    `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
-    Name           string    `gorm:"type:text;not null" json:"name"`
-    SortName       string    `gorm:"type:text" json:"sort_name,omitempty"`
-    Country        string    `gorm:"size:2" json:"country,omitempty"`
-    Type           string    `gorm:"type:text" json:"type,omitempty"` 
-    CreatedAt      time.Time `gorm:"autoCreateTime" json:"-"`
-    UpdatedAt      time.Time `gorm:"autoUpdateTime" json:"-"`
+	ID        string    `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	Name      string    `gorm:"type:text;not null" json:"name"`
+	SortName  string    `gorm:"type:text" json:"sort_name,omitempty"`
+	Country   string    `gorm:"size:2" json:"country,omitempty"`
+	Type      string    `gorm:"type:text" json:"type,omitempty"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"-"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"-"`
+}
+
+func (a *Artist) ToPB() *pb.Artist {
+	return &pb.Artist{
+		Id:       a.ID,
+		Country:  &a.Country,
+		Name:     a.Name,
+		SortName: &a.SortName,
+		Type:     &a.Type,
+	}
 }
