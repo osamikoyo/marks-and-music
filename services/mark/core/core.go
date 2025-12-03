e5c0e16026ff8b58d6bcdaa2dc91fa4e31dd200a
osamikoyo <v.pylayev@mail.ru>
2025-12-03 06:29:00 +0500

    update core

--- services/mark/core/core.go
+++ services/mark/core/core.go
@@ -10,7 +10,10 @@ import (
 type Repository interface {
 	CreateReview(ctx context.Context, review *entity.Review) error
 	DeleteReview(ctx context.Context, id uint) error
-	GetReviewsByReleaseID(ctx context.Context, releaseID string) error
+	GetReviewsByReleaseID(ctx context.Context, releaseID string) ([]entity.Review, error)
+	GetMarkByReleaseID(ctx context.Context, releaseID string) (*entity.Mark, error)
+	CreateMark(ctx context.Context, mark *entity.Mark) error
+	UpdateMarkByReleaseID(ctx context.Context, releaseID string, update *entity.Mark) error
 }
 
 type Core struct {
@@ -25,5 +28,81 @@ func NewCore(repo Repository, timeout time.Duration) *Core {
 	}
 }
 
+func (c *Core) context() (context.Context, context.CancelFunc) {
+	return context.WithTimeout(context.Background(), c.timeout)
+}
+
 func (c *Core) CreateReview(releaseID, text, userID string, count int) error {
+	review := entity.NewReview(releaseID, text, userID, count)
+
+	ctx, cancel := c.context()
+	defer cancel()
+
+	if err := c.repo.CreateReview(ctx, review); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Core) GetReviewsByReleaseID(releaeID string) ([]entity.Review, error) {
+	ctx, cancel := c.context()
+	defer cancel()
+
+	reviews, err := c.repo.GetReviewsByReleaseID(ctx, releaeID)
+	if err != nil {
+		return nil, err
+	}
+
+	return reviews, nil
+}
+
+func (c *Core) GetMarkByReleaeID(releaseID string) (*entity.Mark, error) {
+	ctx, cancel := c.context()
+	defer cancel()
+
+	mark, err := c.repo.GetMarkByReleaseID(ctx, releaseID)
+	if err != nil {
+		return nil, err
+	}
+
+	return mark, nil
+}
+
+func (c *Core) CreateMark(releaseID string, value float32) error {
+	ctx, cancel := c.context()
+	defer cancel()
+
+	mark := entity.NewMark(releaseID, value)
+	if err := c.repo.CreateMark(ctx, mark); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Core) RecountMark(releaeID string) error {
+	ctx, cancel := c.context()
+	defer cancel()
+
+	reviews, err := c.repo.GetReviewsByReleaseID(ctx, releaeID)
+	if err != nil {
+		return err
+	}
+
+	sum := 0
+
+	for _, review := range reviews {
+		sum += review.Count
+	}
+
+	count := float32(sum / len(reviews))
+
+	mark := entity.NewMark(releaeID, count)
+
+	if err := c.repo.UpdateMarkByReleaseID(ctx, releaeID, mark); err != nil {
+		return err
+	}
+
+	return nil
 }
