79f17ceb4586e83120e157212530cc134a013c90
osamikoyo <v.pylayev@mail.ru>
2025-12-06 08:25:58 +0500

    add recounter client

---
+++ services/mark/recounter/client.go
@@ -0,0 +1,15 @@
+package recounter
+
+type Client struct{
+	output chan string
+}
+
+func newClient(output chan string) *Client {
+	return &Client{
+		output: output,
+	}
+}
+
+func (c *Client) TryRecount(releaeID string) {
+	c.output <- releaeID
+}
