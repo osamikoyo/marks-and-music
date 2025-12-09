14ee4d5a4a9eb552ba69974322b99b1cdf787f36
osamikoyo <v.pylayev@mail.ru>
2025-12-09 07:19:58 +0500

    update entity mark

--- services/mark/entity/mark.go
+++ services/mark/entity/mark.go
@@ -1,6 +1,10 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"github.com/osamikoyo/music-and-marks/services/mark/api/proto/gen/pb"
+)
 
 type Mark struct {
 	ID        uint `gorm:"primaryKey" json:"id"`
@@ -16,5 +20,14 @@ func NewMark(releaeID string, value float32) *Mark {
 		ReleaseID: releaeID,
 		Value:     value,
 		CreatedAt: time.Now(),
+	}
+}
+
+func (m *Mark) ToPB() *pb.Mark {
+	return &pb.Mark{
+		Id:        uint64(m.ID),
+		ReleaseId: m.ReleaseID,
+		Value:     m.Value,
+		Reviews:   int32(m.Reviews),
 	}
 }
