613f7475602f74b2b1ac2b748cc83542692e4a60
osamikoyo <v.pylayev@mail.ru>
2025-12-09 07:19:48 +0500

    update mark core

--- services/mark/core/core.go
+++ services/mark/core/core.go
@@ -11,19 +11,26 @@ type Repository interface {
 	CreateReview(ctx context.Context, review *entity.Review) error
 	DeleteReview(ctx context.Context, id uint) error
 	GetReviewsByReleaseID(ctx context.Context, releaseID string) ([]entity.Review, error)
+	GetReviewByID(ctx context.Context, id uint) (*entity.Review, error)
 	GetMarkByReleaseID(ctx context.Context, releaseID string) (*entity.Mark, error)
 	CreateMark(ctx context.Context, mark *entity.Mark) error
 	UpdateMarkByReleaseID(ctx context.Context, releaseID string, update *entity.Mark) error
 }
 
-type Cache interface{
+type Cache interface {
 	Set(key string, value interface{})
 	GetReviews(key string) ([]entity.Review, error)
 }
 
+type Recounter interface {
+	TryRecount(releaseID string)
+}
+
 type Core struct {
-	repo    Repository
-	timeout time.Duration
+	repo      Repository
+	cache     Cache
+	recounter Recounter
+	timeout   time.Duration
 }
 
 func NewCore(repo Repository, timeout time.Duration) *Core {
@@ -47,19 +54,56 @@ func (c *Core) CreateReview(releaseID, text, userID string, count int) error {
 		return err
 	}
 
+	reviews, err := c.cache.GetReviews(releaseID)
+	if err != nil {
+		return err
+	}
+
+	newrevs := make([]entity.Review, len(reviews)+1)
+	newrevs[len(reviews)+1] = *review
+
+	c.cache.Set(releaseID, newrevs)
+
+	c.recounter.TryRecount(releaseID)
+
 	return nil
 }
 
-func (c *Core) GetReviewsByReleaseID(releaeID string) ([]entity.Review, error) {
+func (c *Core) GetReviewsByReleaseID(releaseID string) ([]entity.Review, error) {
 	ctx, cancel := c.context()
 	defer cancel()
 
-	reviews, err := c.repo.GetReviewsByReleaseID(ctx, releaeID)
+	reviews, err := c.cache.GetReviews(releaseID)
+	if err == nil {
+		return reviews, nil
+	}
+
+	reviews, err = c.repo.GetReviewsByReleaseID(ctx, releaseID)
 	if err != nil {
 		return nil, err
 	}
 
+	c.cache.Set(releaseID, reviews)
+
 	return reviews, nil
+}
+
+func (c *Core) DeleteReview(id uint) error {
+	ctx, cancel := c.context()
+	defer cancel()
+
+	review, err := c.repo.GetReviewByID(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	if err := c.repo.DeleteReview(ctx, id); err != nil {
+		return err
+	}
+
+	c.recounter.TryRecount(review.ReleaseID)
+
+	return nil
 }
 
 func (c *Core) GetMarkByReleaeID(releaseID string) (*entity.Mark, error) {
