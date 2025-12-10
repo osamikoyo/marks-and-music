59f93c3b83070428dd58bd63f87e523c4efdd60a
osamikoyo <v.pylayev@mail.ru>
2025-12-10 08:00:17 +0500

    update review entity

--- services/mark/entity/review.go
+++ services/mark/entity/review.go
@@ -1,6 +1,10 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"github.com/osamikoyo/music-and-marks/services/mark/api/proto/gen/pb"
+)
 
 type Review struct {
 	ID        uint      `gorm:"primarKey" json:"id"`
@@ -18,5 +22,15 @@ func NewReview(releaeID, text, userID string, count int) *Review {
 		UserID:    userID,
 		Count:     count,
 		CreatedAt: time.Now(),
+	}
+}
+
+func (r *Review) ToPB() *pb.Review {
+	return &pb.Review{
+		Id: uint64(r.ID),
+		Text: r.Text,
+		Count: int32(r.Count),
+		UserId: r.UserID,
+		ReleaseId: r.ReleaseID,
 	}
 }
