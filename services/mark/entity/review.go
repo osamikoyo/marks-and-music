7f0f314d7090582d034903ba0c45000edb83ee5e
osamikoyo <v.pylayev@mail.ru>
2025-12-12 08:04:18 +0500

    update review entity

--- services/mark/entity/review.go
+++ services/mark/entity/review.go
@@ -27,10 +27,10 @@ func NewReview(releaeID, text, userID string, count int) *Review {
 
 func (r *Review) ToPB() *pb.Review {
 	return &pb.Review{
-		Id: uint64(r.ID),
-		Text: r.Text,
-		Count: int32(r.Count),
-		UserId: r.UserID,
+		Id:        uint64(r.ID),
+		Text:      r.Text,
+		Count:     int32(r.Count),
+		UserId:    r.UserID,
 		ReleaseId: r.ReleaseID,
 	}
 }
