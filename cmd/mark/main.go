881678e5f91ba3d0601fda441acdf70c66ef7264
osamikoyo <v.pylayev@mail.ru>
2025-12-13 14:01:16 +0500

    add mark service cmd

---
+++ cmd/mark/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/osamikoyo/music-and-marks/services/mark/app"
+)
+
+func main() {
+	configpath := ""
+
+	for i, arg := range os.Args{
+		if arg == "--config" {
+			configpath = os.Args[i+1]
+		}
+	}
+
+	app, err := app.SetupApp(configpath)
+	if err != nil{
+		log.Fatal(err)
+
+		return
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	if err := app.Run(ctx);err != nil{
+		log.Fatal(err)
+
+		return
+	}
+}
