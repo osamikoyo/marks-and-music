e9a8d37a6140d16d45513c0e878ff8844af54b7f
osamikoyo <v.pylayev@mail.ru>
2025-12-15 07:53:37 +0500

    add get reviews handler

---
+++ services/api/pkg/mark/handler/get_reviews.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (h *Handler) GetReviews(c echo.Context) error {
+	releaseid := c.Param("releaseid")
+
+	ctx := c.Request().Context()
+
+	reviews, err := h.cc.GetReviews(ctx, releaseid)
+	if err != nil{
+		return c.String(http.StatusInternalServerError, "failed get reviews " + err.Error())
+	}
+
+	return c.JSON(http.StatusOK, reviews)
+}
