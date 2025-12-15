a32ce534126ee5d97cd13666a1cc07c727805333
osamikoyo <v.pylayev@mail.ru>
2025-12-15 07:53:17 +0500

    add get mark handler

---
+++ services/api/pkg/mark/handler/get_mark.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (h *Handler) GetMark(c echo.Context) error {
+	releaseId := c.Param("releaseid")
+
+	ctx := c.Request().Context()
+
+	mark, err := h.cc.GetMark(ctx, releaseId)
+	if err != nil{
+		return c.String(http.StatusInternalServerError, "failed get mark: " + err.Error())
+	}
+
+	return c.JSON(http.StatusOK, mark)
+}
