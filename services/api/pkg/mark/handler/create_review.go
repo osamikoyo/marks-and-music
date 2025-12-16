281e78c889f74757e1bbc738057e2b3d1ad36e5d
osamikoyo <v.pylayev@mail.ru>
2025-12-16 08:12:08 +0500

    add create_review handler

---
+++ services/api/pkg/mark/handler/create_review.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/osamikoyo/music-and-marks/services/mark/entity"
+)
+
+func (h *Handler) CreateReview(c echo.Context) error {
+	var review entity.Review
+
+	if err := c.Bind(&review);err != nil{
+		return c.String(http.StatusBadRequest, "faield bind review")
+	}
+
+	if err := h.cc.CreateReview(c.Request().Context(), &review);err != nil{
+		return c.String(http.StatusInternalServerError, "failed create review " + err.Error())
+	}
+
+	return c.String(http.StatusCreated, "created successfully")
+}
