2eb34d4375925cccbaed2f7a6d17e99cf9299094
osamikoyo <v.pylayev@mail.ru>
2025-12-17 05:15:08 +0500

    update get_user handler

--- services/api/pkg/user/handler/get_user.go
+++ services/api/pkg/user/handler/get_user.go
@@ -10,10 +10,9 @@ func (h *Handler) GetUser(c echo.Context) error {
 	id := c.Param("id")
 
 	user, err := h.cc.GetUser(c.Request().Context(), id)
-	if err != nil{
-		return c.String(http.StatusInternalServerError, "failed fetch user " + err.Error())
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "failed fetch user "+err.Error())
 	}
 
 	return c.JSON(http.StatusOK, user)
 }
-
