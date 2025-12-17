1bbfed0ec1cd59021527238e1180788a583539ef
osamikoyo <v.pylayev@mail.ru>
2025-12-17 05:14:31 +0500

    update change_password handler

--- services/api/pkg/user/handler/change_password.go
+++ services/api/pkg/user/handler/change_password.go
@@ -9,17 +9,17 @@ import (
 func (h *Handler) ChangePassword(c echo.Context) error {
 	id := c.Param("id")
 
-	var req struct{
+	var req struct {
 		Old string `json:"old"`
 		New string `json:"new"`
 	}
 
-	if err := c.Bind(req);err != nil{
+	if err := c.Bind(req); err != nil {
 		return c.String(http.StatusBadRequest, "failed bind passwords")
 	}
 
-	if err := h.cc.ChangePassword(c.Request().Context(), id, req.Old, req.New);err != nil {
-		return c.String(http.StatusBadRequest, "failed change password " + err.Error())
+	if err := h.cc.ChangePassword(c.Request().Context(), id, req.Old, req.New); err != nil {
+		return c.String(http.StatusBadRequest, "failed change password "+err.Error())
 	}
 
 	return c.String(http.StatusOK, "password changed successfully")
