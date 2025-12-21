a95c7ba0f456983555528ec68000f23ca651476f
osamikoyo <v.pylayev@mail.ru>
2025-12-21 08:56:02 +0500

    update get artist handler

--- services/api/pkg/music/handler/get_artist.go
+++ services/api/pkg/music/handler/get_artist.go
@@ -10,8 +10,8 @@ func (h *Handler) GetArtist(c echo.Context) error {
 	id := c.Param("id")
 
 	artist, err := h.cc.GetArtist(c.Request().Context(), id)
-	if err != nil{
-		return c.String(http.StatusInternalServerError, "failed get artist " + err.Error())
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "failed get artist "+err.Error())
 	}
 
 	return c.JSON(http.StatusOK, artist)
