ad4402114ae57380e4c31ebeed8e971f3f07e92b
osamikoyo <v.pylayev@mail.ru>
2025-12-23 06:48:01 +0500

    add api cmd

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/osamikoyo/music-and-marks/services/api/server"
+)
+
+func main() {
+	configPath := "api-config.yaml"
+	for i, arg := range os.Args {
+		if arg == "--config" {
+			configPath = os.Args[i+1]
+		}
+	}
+
+	server, err := server.SetupApiServer(configPath)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+	go func() {
+		<-ctx.Done()
+
+		server.Close(ctx)
+	}()
+
+	if err := server.Start(ctx); err != nil {
+		log.Fatal(err)
+	}
+}
+
